70a0e32f7f62931d302b9c5a417d8f90f629707e
Kevin Peng <peng.kevin@me.com>
2021-05-02 20:29:57 -0400

    Improve 1409

--- Sources/LeetCodeSolutions/1409.swift
+++ Sources/LeetCodeSolutions/1409.swift
@@ -8,14 +8,15 @@
 import Foundation
 class Solution1409 {
     func processQueries(_ queries: [Int], _ m: Int) -> [Int] {
-        var p = Array(1...m)
+        var p = Array(stride(from: m, through: 1, by: -1))
         var result = [Int](repeating: 0, count: queries.count)
         for i in 0 ..< queries.count {
             let currentQuery = queries[i]
-            let index = p.firstIndex(of: currentQuery)!
+            let actualIndex = p.firstIndex(of: currentQuery)!
+            let index = m - actualIndex - 1
             result[i] = index
-            p.remove(at: index)
-            p.insert(currentQuery, at: 0)
+            p.remove(at: actualIndex)
+            p.append(currentQuery)
         }
         return result
     }
