eb8480a4d61d7d1a35b556069d3c49d460d367c1
Kevin Peng <peng.kevin@me.com>
2021-05-08 18:11:39 -0400

    Add 0797

---
+++ Sources/LeetCodeSolutions/0797.swift
@@ -0,0 +1,49 @@
+//
+//  Created by Kevin Peng on 2021-05-08.
+//
+
+import Foundation
+
+
+/*
+ Test cases:
+ [[1,2],[3],[3],[]]
+ [[4,3,1],[3,2,4],[3],[4],[]]
+ [[1],[]]
+ [[1,2,3],[2],[3],[]]
+ [[1,3],[2],[3],[]]
+
+ Expected:
+ [[0,1,3],[0,2,3]]
+ [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+ [[0,1]]
+ [[0,1,2,3],[0,2,3],[0,3]]
+ [[0,1,2,3],[0,3]]
+ */
+struct Q0797 {
+    class Solution {
+        func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+            findPath(graph, 0)
+        }
+
+        func findPath(_ graph: [[Int]], _ currentIndex: Int) -> [[Int]] {
+            if currentIndex == graph.count - 1 {
+                return [[currentIndex]]
+            } else if graph[currentIndex].isEmpty {
+                return []
+            }
+
+            var result = [[Int]]()
+
+            for vertex in graph[currentIndex] {
+                let path = findPath(graph, vertex)
+                if !path.isEmpty {
+                    result += path
+                }
+            }
+            return result.map {
+                [currentIndex] + $0
+            }
+        }
+    }
+}
