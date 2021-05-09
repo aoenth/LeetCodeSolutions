5be48989982369675b3be32bd5a59c12881fa6da
Kevin Peng <peng.kevin@me.com>
2021-05-09 15:43:00 -0400

    Add suggested approach for 0763

--- Sources/LeetCodeSolutions/0763.swift
+++ Sources/LeetCodeSolutions/0763.swift
@@ -6,7 +6,7 @@ import Foundation
 
 struct Q0763 {
     class Solution {
-        func partitionLabels(_ S: String) -> [Int] {
+        func partitionLabels2(_ S: String) -> [Int] {
             let input = Array(S)
             if S.count == 1 {
                 return [1]
@@ -27,6 +27,29 @@ struct Q0763 {
                 }
             }
             result.append(currentIndex - startIndex)
+            return result
+        }
+
+        /**
+         Suggested "Greedy approach" from LeetCode
+         */
+        func partitionLabels(_ S: String) -> [Int] {
+            let input = Array(S)
+            var lastIndices = [Character: Int]()
+            for character in input {
+                lastIndices[character] = input.lastIndex(of: character)!
+            }
+            var result = [Int]()
+            var currentIndex = 0
+            var anchor = 0
+            for i in 0 ..< input.count {
+                let currentCharacter = input[i]
+                currentIndex = max(currentIndex, lastIndices[currentCharacter]!)
+                if i == currentIndex {
+                    result.append(i - anchor + 1)
+                    anchor = i + 1
+                }
+            }
             return result
         }
     }
