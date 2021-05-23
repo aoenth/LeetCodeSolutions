7ed511a19cce6c3f4fa9078c6a7b37988c72e513
Kevin Peng <peng.kevin@me.com>
2021-05-23 18:05:26 -0400

    Improve 1347

--- Sources/LeetCodeSolutions/1347.swift
+++ Sources/LeetCodeSolutions/1347.swift
@@ -6,18 +6,18 @@ import Foundation
 struct Q1347 {
     typealias Answer = Solution
 
-    //236ms, 16MB
+    // O(N) time (280ms), O(1) space (14.9MB)
+    // time = N + 26
+    // space = 26 * 2 + 1 (start) + 1 (common)
     class Solution {
         func minSteps(_ s: String, _ t: String) -> Int {
-            let stringA = Array(s)
-            let stringB = Array(t)
             var lhs = [Int](repeating: 0, count: 26)
             var rhs = [Int](repeating: 0, count: 26)
             var commons = 0
             let start = Character("a").asciiValue!
-            for i in 0 ..< s.count {
-                lhs[Int(stringA[i].asciiValue! - start)] += 1
-                rhs[Int(stringB[i].asciiValue! - start)] += 1
+            for (sc, tc) in zip(s, t) {
+                lhs[Int(sc.asciiValue! - start)] += 1
+                rhs[Int(tc.asciiValue! - start)] += 1
             }
             for i in 0 ..< 26 {
                 if lhs[i] > 0 && rhs[i] > 0 {
